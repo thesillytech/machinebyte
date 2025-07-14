4f11f8491560917a4981dcc763ea97ee2892958c
Sanil Khamkar <sanilpsy@gmail.com>
2025-07-14 18:36:11 -0500

    cleaned stuff

--- components/layout.js
+++ components/layout.js
@@ -1,9 +1,7 @@
-import Head from 'next/head';
 import Image from 'next/image';
 import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
-import Header from '../components/header'
 import Box from '@mui/material/Box'
 
  
@@ -13,38 +11,9 @@ export const siteTitle = 'Next.js Sample Website';
 export default function Layout({ children, home }) {
   return (
     <Box>
-    <Header />
 
     <div className={styles.container}>
-      <Head>
-        <link rel="icon" href="/favicon.ico" />
-        <meta
-          name="description"
-          content="Learn how to build a personal website using Next.js"
-        />
-        <meta
-          property="og:image"
-          content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle,
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-        />
-        <meta name="og:title" content={siteTitle} />
-        <meta name="twitter:card" content="summary_large_image" />
-      </Head>
       <header className={styles.header}>
-        {home ? (
-          <>
-            <Image
-              priority
-              src="/images/profile.jpg"
-              className={utilStyles.borderCircle}
-              height={144}
-              width={144}
-              alt=""
-            />
-            <h1 className={utilStyles.heading2Xl}>{name}</h1>
-          </>
-        ) : (
           <>
             <Link href="/">
               <Image
@@ -62,7 +31,6 @@ export default function Layout({ children, home }) {
               </Link>
             </h2>
           </>
-        )}
       </header>
       <main>{children}</main>
       {!home && (
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,16 @@
 import Head from 'next/head';
-import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 import { getSortedPostsData } from '../lib/posts';
 import Link from 'next/link';
 import Date from '../components/date';
+import Header from '../components/header'
+import styles from '../components/layout.module.css';
 
 export default function Home({ allPostsData }) {
   return (
-    <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
-      </section>
+    <>
+    <Header />
+    <div className={styles.container}>
 
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
@@ -33,7 +26,8 @@ export default function Home({ allPostsData }) {
           ))}
         </ul>
       </section>
-    </Layout>
+      </div>
+    </>
   );
 }
 
